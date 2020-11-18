350dd2a030668db20ba2694b5fd7a3a587e3a1c9
Long <letrunglong07@gmail.com>
2020-11-18 20:55:29 +0700

    asda

--- src/App.js
+++ src/App.js
@@ -2,7 +2,16 @@ import './App.css';
 import { Button, Row, Col, Input } from 'antd';
 import axios from 'axios';
 import { Component } from 'react';
+import './App.css';
 
+
+// module.exports = override(
+//     fixBabelImports('import', {
+//         libraryName: 'antd',
+//         libraryDirectory: 'es',
+//         style: 'css',
+//     }),
+// );
 
 const api = axios.create({
   baseURL: 'http://localhost:5000/api/commands/'
@@ -43,53 +52,63 @@ class App extends Component {
     this.setState({ courses: data })
   }
   createCourse = async () => {
-    if(this.state.how ==="" ||this.state.plat === ""||this.state.command === ""){
+    // if(this.state.courses.howto === this.state.how){
+    //   alert('Use difference title')
+    // }else
+    if (this.state.how === "" || this.state.plat === "" || this.state.command === "") {
       alert('Value not be null')
-    }else{
+    } else {
       let res = await api.post('/', { howto: this.state.how, platform: this.state.plat, commandline: this.state.command })
       this.getApi();
     }
-    
+
   }
   deleteCourse = async (id) => {
     let data = await api.delete(`/${id}`)
     this.getApi();
   }
 
-  updateCourse = async (id,value) => {
-    let data = await api.patch(`/${id}`,{howto: value})
+  updateCourse = async (id, value) => {
+    let data = await api.patch(`/${id}`, { howto: value })
     this.getApi()
   }
   render() {
-
-
-    console.log(this.state.how);
-    console.log(this.state.plat);
-    console.log(this.state.command);
+    console.log(this.state.courses.howto);
     return (
       <div className="container">
-        <Row className="ant-col-24">
-          <Col span={16}>
+        <Row className="row">
+          <Col span={8} className="">
             <Input placeholder="howto..." onChange={this.handleChangeHowto} value={this.state.how}
             />
             <Input placeholder="platform..." onChange={this.handleChangePlatform} value={this.state.plat} />
             <Input placeholder="commandline..." onChange={this.handleChangeCommand} value={this.state.command} />
-            <Button type="primary" onClick={this.createCourse}>ADD NEW</Button>
+            <Button type="primary" onClick={this.createCourse} style={{ marginTop: '10px' }}>ADD NEW</Button>
           </Col>
-          <Col span={8}>
+          <Col span={16} className="main-content">
             {this.state.courses.map(courses =>
-              <div style={{ background: 'aqua' }}>
-                <h3 key={courses.id}> title:{courses.howto}</h3>
-                <p>{courses.platform}</p>
-                <p>{courses.commandline}</p>
-                <Button
-                  onClick={() => this.deleteCourse(courses.id)}>
-                  Delete
+              <div className="one-item">
+                <div className="title">
+                  <div>TITLE:</div>
+                  <div key={courses.id}>{courses.howto}</div>
+                </div>
+                <div className="content">
+                  <div>Content:</div>
+                  <p>{courses.platform}</p>
+                </div>
+                <div className="command">
+                  <div>Description:</div>
+                  <p>{courses.commandline}</p>
+                </div>
+                <div className="bottom">
+                  <Button type='primary' className='but-1 but'
+                    onClick={() => this.deleteCourse(courses.id)}>
+                    Delete
                 </Button>
-                <Button
-                  onClick={() => this.updateCourse(courses.id,`${courses.howto}asdasd`)}>
-                  Update
+                  <Button type='default' className='but-2 but'
+                    onClick={() => this.updateCourse(courses.id, `${courses.howto}asdasd`)}>
+                    Update
                 </Button>
+                </div>
               </div>
             )}
           </Col>
