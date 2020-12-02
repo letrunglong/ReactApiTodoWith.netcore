453ea308af92a82c6fdc9f985bb2cf50bf0ea7ae
Long <letrunglong07@gmail.com>
2020-12-02 20:53:20 +0700

    2

--- src/App.js
+++ src/App.js
@@ -1,123 +1,78 @@
 import './App.css';
-import { Button, Row, Col, Input } from 'antd';
+
 import axios from 'axios';
 import { Component } from 'react';
 import './App.css';
+import IndexScreens from './Component/IndexScreens';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
-
-// module.exports = override(
-//     fixBabelImports('import', {
-//         libraryName: 'antd',
-//         libraryDirectory: 'es',
-//         style: 'css',
-//     }),
-// );
 
 const api = axios.create({
   baseURL: 'http://localhost:5000/api/commands/'
 })
 class App extends Component {
-
-  constructor(props) {
-    super(props);
-
-    this.getApi();
-    this.state = {
-      courses: [],
-      how: '',
-      plat: '',
-      command: ''
-    }
-    this.handleChangeHowto = this.handleChangeHowto.bind(this);
-    this.handleChangePlatform = this.handleChangePlatform.bind(this);
-    this.handleChangeCommand = this.handleChangeCommand.bind(this);
-  }
-  handleChangeHowto(event) {
-    this.setState({
-      how: event.target.value
-    });
-  }
-  handleChangePlatform(event) {
-    this.setState({
-      plat: event.target.value
-    });
-  }
-  handleChangeCommand(event) {
-    this.setState({
-      command: event.target.value
-    });
-  }
-  getApi = async () => {
-    let data = await api.get('/').then(({ data }) => data);
-    this.setState({ courses: data })
-  }
-  createCourse = async () => {
-    // if(this.state.courses.howto === this.state.how){
-    //   alert('Use difference title')
-    // }else
-    if (this.state.how === "" || this.state.plat === "" || this.state.command === "") {
-      alert('Value not be null')
-    } else {
-      let res = await api.post('/', { howto: this.state.how, platform: this.state.plat, commandline: this.state.command })
-      this.getApi();
-    }
-
-  }
-  deleteCourse = async (id) => {
-    let data = await api.delete(`/${id}`)
-    this.getApi();
-  }
-
-  updateCourse = async (id, value) => {
-    let data = await api.patch(`/${id}`, { howto: value })
-    this.getApi()
-  }
   render() {
-    console.log(this.state.courses.howto);
     return (
       <div className="container">
-        <Row className="row">
-          <Col span={8} className="">
-            <Input placeholder="howto..." onChange={this.handleChangeHowto} value={this.state.how}
-            />
-            <Input placeholder="platform..." onChange={this.handleChangePlatform} value={this.state.plat} />
-            <Input placeholder="commandline..." onChange={this.handleChangeCommand} value={this.state.command} />
-            <Button type="primary" onClick={this.createCourse} style={{ marginTop: '10px' }}>ADD NEW</Button>
-          </Col>
-          <Col span={16} className="main-content">
-            {this.state.courses.map(courses =>
-              <div className="one-item">
-                <div className="title">
-                  <div>TITLE:</div>
-                  <div key={courses.id}>{courses.howto}</div>
-                </div>
-                <div className="content">
-                  <div>Content:</div>
-                  <p>{courses.platform}</p>
-                </div>
-                <div className="command">
-                  <div>Description:</div>
-                  <p>{courses.commandline}</p>
-                </div>
-                <div className="bottom">
-                  <Button type='primary' className='but-1 but'
-                    onClick={() => this.deleteCourse(courses.id)}>
-                    Delete
-                </Button>
-                  <Button type='default' className='but-2 but'
-                    onClick={() => this.updateCourse(courses.id, `${courses.howto}asdasd`)}>
-                    Update
-                </Button>
-                </div>
-              </div>
-            )}
-          </Col>
-        </Row>
+        <Router>
+          <div>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/about">About</Link>
+              </li>
+              <li>
+                <Link to="/dashboard">Dashboard</Link>
+              </li>
+            </ul>
+            <hr />
+            <Switch>
+              <Route exact path="/">
+                <IndexScreens />
+              </Route>
+              <Route path="/about">
+                <About />
+              </Route>
+              <Route path="/dashboard">
+                <Dashboard />
+              </Route>
+            </Switch>
+          </div>
+        </Router>
       </div>
-
     );
   }
 
+}
+function Home() {
+  return (
+    <div>
+      <h2>Home</h2>
+    </div>
+  );
+}
+
+function About() {
+  return (
+    <div>
+      <h2>About</h2>
+    </div>
+  );
+}
+
+function Dashboard() {
+  return (
+    <div>
+      <h2>Dashboard</h2>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Component/IndexScreens.js
@@ -0,0 +1,107 @@
+import React, { Component } from 'react';
+import { Button, Row, Col, Input } from 'antd';
+import axios from 'axios';
+
+const api = axios.create({
+  baseURL: 'http://localhost:5000/api/commands/'
+})
+
+class IndexScreens extends Component {
+  constructor(props) {
+    super(props);
+
+    this.getApi();
+    this.state = {
+      courses: [],
+      how: '',
+      plat: '',
+      command: ''
+    }
+    this.handleChangeHowto = this.handleChangeHowto.bind(this);
+    this.handleChangePlatform = this.handleChangePlatform.bind(this);
+    this.handleChangeCommand = this.handleChangeCommand.bind(this);
+  }
+  handleChangeHowto(event) {
+    this.setState({
+      how: event.target.value
+    });
+  }
+  handleChangePlatform(event) {
+    this.setState({
+      plat: event.target.value
+    });
+  }
+  handleChangeCommand(event) {
+    this.setState({
+      command: event.target.value
+    });
+  }
+  getApi = async () => {
+    let data = await api.get('/').then(({ data }) => data);
+    this.setState({ courses: data })
+  }
+  createCourse = async () => {
+    if (this.state.how === "" || this.state.plat === "" || this.state.command === "") {
+      alert('Value not be null')
+    } else {
+      let res = await api.post('/', { howto: this.state.how, platform: this.state.plat, commandline: this.state.command })
+      this.getApi();
+    }
+
+  }
+  deleteCourse = async (id) => {
+    let data = await api.delete(`/${id}`)
+    this.getApi();
+  }
+
+  updateCourse = async (id, value) => {
+    let data = await api.patch(`/${id}`, { howto: value })
+    this.getApi()
+  }
+  render() {
+    return (
+      <div className='index main'>
+        <h2>Home</h2>
+        <Row className="row">
+          <Col span={8} className="">
+            <Input placeholder="howto..." onChange={this.handleChangeHowto} value={this.state.how}
+            />
+            <Input placeholder="platform..." onChange={this.handleChangePlatform} value={this.state.plat} />
+            <Input placeholder="commandline..." onChange={this.handleChangeCommand} value={this.state.command} />
+            <Button type="primary" onClick={this.createCourse} style={{ marginTop: '10px' }}>ADD NEW</Button>
+          </Col>
+          <Col span={16} className="main-content">
+            {this.state.courses.map(courses =>
+              <div className="one-item">
+                <div className="title">
+                  <div>TITLE:</div>
+                  <div key={courses.id}>{courses.howto}</div>
+                </div>
+                <div className="content">
+                  <div>Content:</div>
+                  <p>{courses.platform}</p>
+                </div>
+                <div className="command">
+                  <div>Description:</div>
+                  <p>{courses.commandline}</p>
+                </div>
+                <div className="bottom">
+                  <Button type='primary' className='but-1 but'
+                    onClick={() => this.deleteCourse(courses.id)}>
+                    Delete
+                </Button>
+                  <Button type='default' className='but-2 but'
+                    onClick={() => this.updateCourse(courses.id, `${courses.howto}asdasd`)}>
+                    Update
+                </Button>
+                </div>
+              </div>
+            )}
+          </Col>
+        </Row>
+      </div>
+    );
+  }
+}
+
+export default IndexScreens;
